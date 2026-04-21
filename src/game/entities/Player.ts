fc56339aea359c5bf7e1b349073a57b68fd3e93e
Arnel-rah <arnel.raharinandrasana@student.passerellesnumeriques.org>
2026-04-21 10:53:43 +0300

    fix(game): resolve TS type errors and update obsolete Graphics methods

--- src/game/entities/Player.ts
+++ src/game/entities/Player.ts
@@ -1,49 +1,54 @@
 import * as Phaser from "phaser";
 
-export class Player extends Phaser.Physics.Arcade.Sprite {
-  private cursors!: Phaser.Types.Input.Keyboard.CursorKeys;
+export class Player {
+  private scene: Phaser.Scene;
+  private sprite!: Phaser.Physics.Arcade.Sprite;
+  private health: number = 100;
+  private score: number = 0;
 
   constructor(scene: Phaser.Scene, x: number, y: number) {
-    super(scene, x, y, "robot");
-
-    scene.add.existing(this);
-    scene.physics.add.existing(this, false);
-
-    this.setCollideWorldBounds(true);
-    this.setBounce(0.1);
-    this.setDragX(800);
-    if (!scene.textures.exists("robot")) {
-      this.createPlaceholderTexture(scene);
-    }
-
-    this.cursors = scene.input.keyboard!.createCursorKeys();
+    this.scene = scene;
+    this.createPlayer(x, y);
   }
 
-  private createPlaceholderTexture(scene: Phaser.Scene) {
-    const gfx = scene.make.graphics({ x: 0, y: 0 });
-    gfx.fillStyle(0x00ccff, 1);
-    gfx.fillRect(0, 0, 42, 52);
-    gfx.fillStyle(0xff8800, 1);
-    gfx.fillRect(12, 8, 18, 18);
-    gfx.generateTexture("robot", 42, 52);
-    gfx.destroy();
+  private createPlayer(x: number, y: number) {
+    // Create a simple player sprite
+    const graphics = this.scene.make.graphics({ x: 0, y: 0});
+    graphics.fillStyle(0x00d4ff, 1);
+    graphics.fillCircle(16, 16, 12);
+    graphics.generateTexture("player", 32, 32);
+    graphics.destroy();
+
+    this.sprite = this.scene.physics.add.sprite(x, y, "player");
+    this.sprite.setBounce(0.2);
+    this.sprite.setCollideWorldBounds(true);
   }
 
-  update() {
-    const body = this.body as Phaser.Physics.Arcade.Body;
+  public getSprite(): Phaser.Physics.Arcade.Sprite {
+    return this.sprite;
+  }
 
-    if (this.cursors.left.isDown) {
-      this.setVelocityX(-280);
-      this.setFlipX(true);
-    } else if (this.cursors.right.isDown) {
-      this.setVelocityX(280);
-      this.setFlipX(false);
-    } else {
-      this.setVelocityX(0);
-    }
+  public getHealth(): number {
+    return this.health;
+  }
 
-    if (this.cursors.up.isDown && body.onFloor()) {
-      this.setVelocityY(-520);
-    }
+  public takeDamage(amount: number) {
+    this.health -= amount;
+  }
+
+  public heal(amount: number) {
+    this.health = Math.min(100, this.health + amount);
+  }
+
+  public addScore(points: number) {
+    this.score += points;
+  }
+
+  public getScore(): number {
+    return this.score;
+  }
+
+  public destroy() {
+    this.sprite.destroy();
   }
 }
